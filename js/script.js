045f5fad7766bfed4011f2e4df069d170fb16817
Andrey Minakin <minakin.andrey@gmail.com>
2019-05-21 14:24:02 +0200

    90%

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 var menuShow = document.querySelector('.menu-button');
-var secondMenuShow = document.querySelector('.second-menu-show')
+var writeUsShow = document.querySelector('.callback-button');
+var secondMenuShow = document.querySelector('.second-menu-show');
 var menu = document.querySelector('.menu-navigation');
-var secondMenu = document.querySelector('.second-level-menu')
+var secondMenu = document.querySelector('.second-level-menu');
+var writeUs = document.querySelector('.modal-write-us');
+var modalClose = document.querySelector('.modal-close');
 
 menuShow.addEventListener('click', function(evt) {
   evt.preventDefault();
   if (menu.classList.contains('menu-navigation-show')) {
-    menu.classList.remove('menu-navigation-show');}
-  else {  menu.classList.add('menu-navigation-show');}
+    menu.classList.remove('menu-navigation-show');
+  secondMenu.classList.remove('second-level-menu-show');}
+  else {menu.classList.add('menu-navigation-show');}
 });
 
 window.addEventListener('keydown', function(evt) {
@@ -15,7 +19,17 @@ window.addEventListener('keydown', function(evt) {
     evt.preventDefault();
     if (menu.classList.contains('menu-navigation-show')) {
       menu.classList.remove('menu-navigation-show');
+      secondMenu.classList.remove('second-level-menu-show');
     }
+    if (writeUs.classList.contains('modal-show')) {
+      writeUs.classList.remove('modal-show');
+    }
+  }
+});
+
+modalClose.addEventListener('click', function(evt) {
+  if (writeUs.classList.contains('modal-show')) {
+    writeUs.classList.remove('modal-show');
   }
 });
 
@@ -23,8 +37,27 @@ secondMenuShow.addEventListener('mouseover', function(evt) {
   evt.preventDefault();
   secondMenu.classList.add('second-level-menu-show');
 });
-//
-//menu.addEventListener('mouseout', function(evt) {
+
+//writeUsShow.addEventListener('click', function(evt) {
 //  evt.preventDefault();
-//  secondMenu.classList.remove('second-level-menu-show');
+//  writeUs.classList.add('modal-show');
 //});
+
+$('#button').click(function(e) {
+    var $message = $('#popup');
+
+    if ($message.css('display') != 'block') {
+        writeUs.classList.add('modal-show');
+
+        var firstClick = true;
+        $(document).bind('click.myEvent', function(e) {
+            if (!firstClick && $(e.target).closest('#popup').length == 0) {
+                writeUs.classList.remove('modal-show');
+                $(document).unbind('click.myEvent');
+            }
+            firstClick = false;
+        });
+    }
+
+    e.preventDefault();
+});
